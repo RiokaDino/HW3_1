de719f09e4c74afe5b3d0523f11b1802e3ebf571
RiokaDino <110732907+RiokaDino@users.noreply.github.com>
2022-11-25 00:32:20 +0700

    HW3_1

---
+++ HW3_1/Program.cs
@@ -0,0 +1,86 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Queue<char> Flower = new Queue<char>();
+        char flower = 'x';
+        int check = 0;
+        char SteeringWheel = 'y';
+        int i = 0;
+        
+            while (flower == 'J' || flower == 'G' || flower == 'O' || flower == 'R' || flower == 'x')
+            {
+                flower = char.Parse(Console.ReadLine());
+            if (flower == 'J')
+            {
+                if (flower != SteeringWheel)
+                {
+                    Flower.Push('J');
+                    check++;
+                    SteeringWheel = 'z';
+                }
+                else
+                {
+                    Console.WriteLine("Invalid patten");
+
+                }
+            }
+            else if(flower == 'G')
+            {
+             
+                    if(flower != SteeringWheel)
+                    {
+                        Flower.Push('G');
+                        SteeringWheel = 'z';
+                        check++;
+                        i++;
+                    }
+                
+                else if(i>3)
+                {
+                    Console.WriteLine("invalid patten");
+    
+                }
+                else
+                {
+                    Console.Write("Invalid patten");
+            
+                }
+            }
+            else if (flower == 'O')
+            {
+                if(flower != SteeringWheel)
+                {
+                    Flower.Push('O');
+                    SteeringWheel = 'z';
+                    check++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid patten");
+                
+                }
+            }
+            else if(flower == 'R')
+            {
+                if(check == 0)
+                {
+                    Console.WriteLine("Invalid patten");
+                }
+                else
+                {
+                    Flower.Push('R');
+                    SteeringWheel = 'z';
+                    SteeringWheel = Flower.Get(Flower.GetLength()-2);
+                    check++;    
+                }
+            }
+        }
+       
+        for(int n = 0;n<Flower.GetLength();n++)
+            {
+                Console.Write("{0}",Flower.Get(n));
+            }
+            
+    }   
+}
